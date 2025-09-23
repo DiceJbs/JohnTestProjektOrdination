3000490970a17899d4d36691e6d46ad04a8cbf9a
Ayaan <eaa24ahah@students.eaaa.dk>
2025-09-23 11:43:33 +0200

    added linkattribut Lægemiddel added metoder for samletDosis & døgnDosis

--- src/ordination/DagligSkæv.java
+++ src/ordination/DagligSkæv.java
@@ -1,8 +1,41 @@
 package ordination;
 
-public class DagligSkæv {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+public abstract class DagligSkæv extends Ordination {
+    private List<Dosis> dosisList;
 
-    private int test = 1;
+    public DagligSkæv(LocalDate startDato, LocalDate slutDato, List<Dosis> dosisList) {
+        super(startDato,slutDato);
+        this.dosisList = dosisList;
+
+    }
+
+    public List<Dosis> getDosisList() {
+        return dosisList;
+    }
+    @Override
+    public double samletDosis() {
+        double totalDosis = 0;
+        for (Dosis dosis : dosisList) {
+            totalDosis += dosis.getAntal();
+        }
+        return totalDosis;
+    }
+    @Override
+    public double døgnDosis() {
+        double døgnDo = 0;
+        for (Dosis dosis : dosisList) {
+            døgnDo += dosis.getAntal();
+        }
+        return døgnDo/antalDage();
+    }
+    @Override
+    public String getType() {
+        return " DagligSkæv ";
+    }
+
 
 }
