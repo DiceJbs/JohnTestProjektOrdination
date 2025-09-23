2dc0c4b392516bf444948402bea7569dd12ec795
Sander Frost <sanderfrost123@gmail.com>
2025-09-23 11:08:48 +0200

    added everything to PN

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,10 +1,16 @@
 package ordination;
-
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
-public class PN {
+public class PN extends Ordination {
     private final double antalEnheder;
+    private ArrayList<LocalDate> datoerHvorOrdinationIndtaget = new ArrayList<>();
 
+    public PN(LocalDate startDato, LocalDate slutDato, double antalEnheder) {
+        super(startDato, slutDato);
+        this.antalEnheder = antalEnheder;
+    }
     public double getAntalEnheder() {
         return antalEnheder;
     }
@@ -12,13 +18,31 @@ public class PN {
     /**
      * Registrer datoen for en anvendt dosis.
      */
+public void registerDate (LocalDate date){
+    datoerHvorOrdinationIndtaget.add(date);
+}
     public void anvendDosis(LocalDate dato) {
+        registerDate(dato);
 
     }
 
     /** Returner antal gange ordinationen er anvendt. */
-    public int antalGangeAnvendt() {
+    public double antalGangeAnvendt() {
+        return datoerHvorOrdinationIndtaget.size();
+    }
 
-        return -1;
+    @Override
+    public double samletDosis() {
+        return antalGangeAnvendt()*antalEnheder;
+    }
+
+    @Override
+    public double døgnDosis() {
+        return (antalGangeAnvendt()*antalEnheder)/ ChronoUnit.DAYS.between(datoerHvorOrdinationIndtaget.getFirst(),datoerHvorOrdinationIndtaget.getLast());
+    }
+
+    @Override
+    public String getType() {
+        return "PN";
     }
 }
